37d53013f2ad2ce53afa33e01347e754db7c5bd4
Fady Adel <2masadel@gmail.com>
2017-08-07 12:08:42 +0200

    last one

--- tests/test_putshebang.py
+++ tests/test_putshebang.py
@@ -23,7 +23,6 @@ class TestPutshebang(unittest.TestCase):
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
-        assert result.exit_code == 0
         assert 'putshebang.cli.main' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert '--help  Show this message and exit.' in help_result.output
