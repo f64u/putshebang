eba5e2712827d05fd61451318a5bc290caa8622c
Fady Adel <2masadel@gmail.com>
2017-08-16 11:55:52 +0200

    bugfix

--- putshebang/__init__.py
+++ putshebang/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from putshebang.putshebang import *
+from .putshebang import *
 """Top-level package for putshebang."""
 
 __author__ = """Fady Adel"""
 __email__ = '2masadel@gmail.com'
 __version__ = '0.1.2'
+
+__all__ = ["ShebangedFile", "UnshebangedFile", "shebang"]
